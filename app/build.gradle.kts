4e550b0a6b547c7d2f13a6c1a2c4f8c00e9beb4c
Paco Andrés <paco.andres.70@gmail.com>
2026-06-18 17:00:07 +0200

    Changes in buttons layout.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.gnu.itsmoroto.midandpad"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("Integer", "dbversion", "2")
     }
