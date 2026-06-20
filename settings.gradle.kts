71d271779bc88d2fdb5bffe381bca50f62548ae1
Paco Andrés <paco.andres.70@gmail.com>
2026-06-20 11:29:04 +0200

    Remove org.gradle.toolchains.foojay-resolver from settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
+/*plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
+}*/
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
